62f727512e84eae416db825a67fcbe8b8b2fb4fb
jay-bhingradiya <jay.bhingradiya@bacancy.com>
2021-08-16 12:48:45 +0530

    deleteMovie action deleted

--- src/redux/movies/actions.js
+++ src/redux/movies/actions.js
@@ -1,25 +1,19 @@
 const movieActions = {
-  ADD_MOVIE: 'ADD_MOVIE',
-  REMOVE_MOVIE: 'REMOVE_MOVIE',
-  EDIT_MOVIE: 'EDIT_MOVIE',
-  DELETE_MOVIE: 'DELETE_MOVIE',
+  ADD_MOVIE: "ADD_MOVIE",
+  REMOVE_MOVIE: "REMOVE_MOVIE",
+  EDIT_MOVIE: "EDIT_MOVIE",
+  DELETE_MOVIE: "DELETE_MOVIE",
 
-  addMovie: data => ({
+  addMovie: (data) => ({
     type: movieActions.ADD_MOVIE,
     payload: data,
   }),
-  deleteMovie: id => {
-    return {
-      type: movieActions.REMOVE_MOVIE,
-      payload: id,
-    };
-  },
 
-  editMovie: data => ({
+  editMovie: (data) => ({
     type: movieActions.EDIT_MOVIE,
     payload: data,
   }),
-  deleteMovie: id => ({
+  deleteMovie: (id) => ({
     type: movieActions.DELETE_MOVIE,
     payload: id,
   }),
